bb92589efaa800929dd383661d60d9305971fe09
sungfire <dlatjdgns424@naver.com>
2023-03-30 18:22:41 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from typing import List
+
+# Function to return a list of even integers from a given list
+def even_list(int_list: List[int]) -> List[int]:
+    """Determines if a number is even and returns an even list.
+    
+    Args:
+        int_list: A list of integers.
+    
+    Returns:
+        A list of even integers.
+    """
+    # TODO: Implement even.list
+    pass
+
+# Function to compute the sum of squares of even integers in a given list
+def sum_of_squares_of_even(even_int_list: List[int]) -> int:
+    """Computes the sum of the squares of all even numbers in a list of integers.
+    
+    Args:
+        even_int_list: A list of even integers.
+    
+    Returns:
+        The sum of the squares of all even numbers in the list.
+    """
+   # TODO: Implement sum_of_squares_of_even
+    pass
+
+# Main function
+def main():
+    # Example list
+    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    even_int_list = even_list(int_list)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
+
+# Boilerplate code
+if __name__ == "__main__":
+    main()
